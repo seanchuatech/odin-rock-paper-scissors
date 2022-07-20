432c0cd14fef0b71137f37dffe481a9cf3f032b5
Leonard Sean <seanchuatech@gmail.com>
2022-07-20 21:04:06 +0800

    initial version of rock paper and scissors

--- js/jscript.js
+++ js/jscript.js
@@ -43,35 +43,100 @@ function game () {
 
     let player = 0;
     let computer = 0;
-    let result;
+    let result = '';
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = window.prompt("What's your move?")
-        const computerSelection = computerPlay();
-        let round = playRound(playerSelection,  computerSelection);
+    // for (let i = 0; i < 5; i++) {
+        // const playerSelection = window.prompt("What's your move?")
+    
+        // let round = playRound(playerSelection,  computerSelection);
 
-        console.log(round);
+        // console.log(round);
 
-        if (round == 1) {
-            player++;
-        } else if (round == 2) {
-            computer++;
-        } else {
-            player++;
-            computer++;
-        }
-    }
+        // if (round == 1) {
+        //     player++;
+        // } else if (round == 2) {
+        //     computer++;
+        // } else {
+        //     player++;
+        //     computer++;
+        // }
+    // }CODE
+        let winner = document.getElementById('winner');
+        let btns = document.querySelectorAll('button');
 
-    if (player > computer) {
-        result = 'YOU WIN!';
-    } else if (computer > player) {
-        result = 'YOU LOSE!';
-    } else {
-        result = 'DRAW';
-    }
+        let round = '';
+        btns.forEach((button) =>
+        button.addEventListener(('click'), () =>{
+            if(button.id) {
+                const computerSelection = computerPlay();
+                round = playRound(button.id, computerSelection)
+                
+                if (round == 1) {
+                    player++;
+                } else if (round == 2) {
+                    computer++;
+                } else {
+                    player++;
+                    computer++;
+                }
+                console.log(player)
+                console.log(computer)
+                
+                if(player === 5 || computer === 5) {
+                    if (player > computer) {
+                        result = 'YOU WIN!';
+                        winner.textContent = result;
+                    } else if (computer > player) {
+                        result = 'YOU LOSE!';
+                        winner.textContent = result;
+                    } else {
+                        result = 'DRAW';
+                        winner.textContent = result;
+                    }
 
-    return result;
+                }
+                
+            }
+        }))
+        return '';
+        
+        
+        
+        // rockBtn.addEventListener("click", function(){
+        //     const computerSelection = computerPlay();
+        //     let round = playRound('rock', computerSelection);
+
+        //     console.log(round);
+
+        //     if (round == 1) {
+        //         player++;
+        //     } else if (round == 2) {
+        //         computer++;
+        //     } else {
+        //         player++;
+        //         computer++;
+        //     }
+        // })
+
+        // paperBtn.addEventListener("click", function(){
+        //     const computerSelection = computerPlay();
+        //     playRound('paper', computerSelection);
+        // });
+
+        // scissorBtn.addEventListener("click", function(){
+        //     const computerSelection = computerPlay();
+        //     playRound('scissor', computerSelection);
+        // });
+
+
+        // if (player > computer) {
+        //     result = 'YOU WIN!';
+        // } else if (computer > player) {
+        //     result = 'YOU LOSE!';
+        // } else {
+        //     result = 'DRAW';
+        // }
+    
 }
 
 console.log(game());
-
